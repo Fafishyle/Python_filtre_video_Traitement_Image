e916d9d1a7e57b23d120f89e0c9b5a2103000f42
Fafishyle <finaritralft@gmail.com>
2023-12-11 16:40:20 +0100

    Mise en place du filtre des yeux

--- VIVIER_RANDRIAMITANDRINA_Projet/test.py
+++ VIVIER_RANDRIAMITANDRINA_Projet/test.py
@@ -2,7 +2,6 @@ import cv2
 from tkinter import *
 from PIL import Image, ImageTk
 
-
 # Charger les images
 sunglasses = cv2.imread('sunglasses.png')
 alpha_sunglasses = cv2.imread('alpha.png')
@@ -14,48 +13,79 @@ cap = cv2.VideoCapture(0)
 master = Tk()
 master.geometry("640x480")  # Ajustez la taille selon vos besoins
 
-# Création de la barre des menu
+# Création de la barre des menus
 menuBar = Menu(master)
 
 # Création du menu principal 'Fichier'
 menuFichier = Menu(menuBar, tearoff=0)
 menuBar.add_cascade(label="Choix des filtres", menu=menuFichier)
+
+# Déclarer la variable globale
 bool_activate_filtre_lunnette = False
+
 def filtre_lunnette():
-   print("hello")
-   bool_activate_filtre_lunnette = True
-# Création des sous menus : 'Filtre lunette de soleil', 'Quitter'
-menuFichier.add_command(label="Filtre lunette de soleil", command= filtre_lunnette)
+    global bool_activate_filtre_lunnette
+    bool_activate_filtre_lunnette = not bool_activate_filtre_lunnette
+
+# Création des sous-menus : 'Filtre lunette de soleil', 'Quitter'
+menuFichier.add_command(label="Activer/Desactiver le filtre lunnette de soleil", command=filtre_lunnette)
 menuFichier.add_command(label="Quitter", command=master.quit)
 
 # Configuration de la barre des menus
 master.config(menu=menuBar)
 
-# Créer un label pour afficher l'image
+# Créer une étiquette pour afficher l'image
 panel = Label(master)
 panel.pack(side="bottom", fill="both", expand="yes")
 
 # Fonction pour mettre à jour l'image
 def update_image():
     ret, frame = cap.read()
-
     if ret:
         p1 = frame.copy()
-
-        # Votre code de détection de visage et d'yeux ici...
-        print(bool_activate_filtre_lunnette)
-        if(bool_activate_filtre_lunnette):
-            p1 = 255 - p1
-        # ... (votre code existant pour détecter les yeux et appliquer les lunettes)
-
+        #______________ FILTRE LUNNETTE____________________
+        if bool_activate_filtre_lunnette:
+            p1_gray = cv2.cvtColor(p1, cv2.COLOR_BGR2GRAY)
+            face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+            eyes_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+            # Detect faces
+            faces = face_cascade.detectMultiScale(p1_gray, 1.1, 4)
+            for (x,y,w,h) in faces :
+                faceROI_gray = p1_gray[y:y+h,x:x+w]
+                eyes = eyes_cascade.detectMultiScale(faceROI_gray, 1.3, 5)
+                # Si on detecte 2 yeux
+                if len(eyes) == 2 :
+                    # Largeur des lunettes
+                    width_sunglasses = abs(eyes[0,0] - (eyes[1,0] + eyes[1,2]))
+                    # Hauteur des lunettes
+                    height_sunglasses = abs (eyes[0,1] - (eyes[1,1] + eyes[1,3]))
+                    # Si les 2 yeux détectés ne sont pas les mêmes
+                    if height_sunglasses != 0 and width_sunglasses != 0 :
+                        # On redimensionne les lunnettes
+                        sunglasses_resize = cv2.resize(sunglasses, (width_sunglasses, height_sunglasses),1,1)
+                        # On redimensionne aussi le masque alpha des lunnettes
+                        alpha_sunglasses_resize = cv2.resize(alpha_sunglasses, (width_sunglasses, height_sunglasses),1,1)
+                        # Calcul du centre des yeux
+                        center_x = int((eyes[0, 0] + eyes[1, 0] + eyes[0, 2] + eyes[1, 2]) / 2)
+                        center_y = int((eyes[0, 1] + eyes[1, 1] + eyes[0, 3] + eyes[1, 3]) / 2)
+                        # Calcul des décalages vertical et horizontal
+                        offset_x = int(width_sunglasses * 0.2)  # Ajustez la valeur selon votre besoin
+                        offset_y = int(height_sunglasses * 0.2)  # Ajustez la valeur selon votre besoin
+                        # Positions de départ des lunettes
+                        debut_x = center_x - int(width_sunglasses / 2) - offset_x
+                        debut_y = center_y - int(height_sunglasses / 2) - offset_y
+                        def filtre_lunnette():
+                                #________Dans cette boucle, j'enlève le fond noir de l'image lunettes grâce au masque alpha_________
+                                for i in range(0,sunglasses_resize.shape[0]) :
+                                    for j in range(0, sunglasses_resize.shape[1]) :
+                                        if alpha_sunglasses_resize[i,j,0] != 0 :
+                                            p1[i+debut_y+y,j+debut_x+x] = sunglasses_resize[i,j]
         # Convertir l'image OpenCV en image Pillow
         img = Image.fromarray(cv2.cvtColor(p1, cv2.COLOR_BGR2RGB))
         img = ImageTk.PhotoImage(image=img)
-
-        # Mettre à jour l'image dans le label
+        # Mettre à jour l'image dans l'étiquette
         panel.configure(image=img)
         panel.image = img
-
     # Planifier la prochaine mise à jour
     master.after(10, update_image)
 
